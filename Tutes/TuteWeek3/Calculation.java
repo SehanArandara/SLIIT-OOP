bb7ac15d26adaee5d751b9a123f90842ef202910
Sehan Arandara <99310968+SehanArandara@users.noreply.github.com>
2022-09-14 23:16:27 +0530

    Add files via upload

---
+++ Tutes/TuteWeek3/Calculation.java
@@ -0,0 +1,14 @@
+package TuteWeek3;
+
+public class Calculation {
+	static int add(int n,int m) {
+		return n+m;
+	}
+	
+	static int sub(int n,int m) {
+		return n-m;
+	}
+	
+}
+
+
---
+++ Tutes/TuteWeek3/DemoApp.java
@@ -0,0 +1,8 @@
+package TuteWeek3;
+
+class DemoApp{
+	public static void main(String[] args) {
+		System.out.println("add :"+Calculation. add(5,10));
+		System.out.println("substract :"+Calculation.sub(5,1));
+	}
+}
