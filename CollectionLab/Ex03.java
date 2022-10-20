80055845c8c19772d8252afeea3c3e529cd63724
Sehan Arandara <99310968+SehanArandara@users.noreply.github.com>
2022-10-20 20:00:24 +0530

    Add files via upload

--- CollectionLab/Ex03.java
+++ CollectionLab/Ex03.java
@@ -1,4 +1,27 @@
+import java.util.HashSet;
+import java.util.Scanner;
 
 public class Ex03 {
-
+	
+	public static void main(String[] args) {
+		HashSet<Double> height =  new HashSet<>();
+		
+		Scanner scn = new Scanner(System.in);
+		
+		double h;
+		for(int i=0;i<10;i++) {
+			System.out.println("enter your height :");
+			height.add(scn.nextDouble());
+		}
+		
+		for(Object obj :height) {
+			System.out.println(obj);
+		}
+		
+		
+		
+		
+	}
+	
+	
 }
